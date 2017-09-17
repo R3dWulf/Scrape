5b84e633dcbdf59dde810095091dde18aa548c53
R3dWulf <theakaiokami@gmail.com>
2017-09-17 13:14:29 -0400

    '

---
+++ public/app.js
@@ -0,0 +1,104 @@
+$("#paper").on('click', function(){
+    $.ajax({
+    method: "GET",
+    url: "/articles",
+    success : function(data){
+        for (var i = 0; i < data.length; i++) {
+          // Display the apropos information on the page
+          // $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+          //console.log("()()()()"+data[i].title );
+           $("#scrape").append(
+              '<div class="row">' +
+                  '<div class="col-md-12">' +           
+                      '<div class="panel-group">' +
+                          '<div class="panel panel-primary">'+
+                              '<div class="panel panel-heading">' + 
+                                  '<p>' + data[i].title + '</p>' + 
+                                  '<button  id="savenote" style="background-color: green" data-id="'+ data[i]._id+'">Save Article</button>' +
+                                 '<a class="waves-effect waves-light btn modal-trigger" href="#modal1">Modal</a>'+
+                              '</div>' + 
+                              '<div class="panel-body">' + 
+                                  '<a href="' + data[i].link + '">' +  data[i].link + '</a>' +
+                              '</div>' +
+                          '<div>' +
+                      '<div>' +
+                  '<div>' +
+              '<div>'
+            );
+        }
+      }
+  });
+});
+
+
+
+// Whenever someone clicks a p tag
+// $(document).on("click", "p", function() {
+//   // Empty the notes from the note section
+//   $("#myModal").empty();
+//   // Save the id from the p tag
+//   var thisId = $(this).attr("data-id");
+
+  // Now make an ajax call for the Article
+  // $.ajax({
+  //   method: "GET",
+  //   url: "/articles/" + thisId
+  // })
+    // With that done, add the note information to the page
+    // .done(function(data) {
+      //console.log(data);
+      // The title of the article
+      // $("#myModal").append(
+      //   '<div class="modal-content">'+
+      //      '<div class="modal-body">'+
+      //         '<p>some text</p>'+
+      //       '</div>'+
+      //   '</div>'
+      // );
+
+      // $("#myModal").append("<h2>" + data.title + "</h2>");
+      // // An input to enter a new title
+      // $("#myModal").append("<input id='titleinput' name='title' >");
+      // // A textarea to add a new note body
+      // $("#myModal").append("<textarea id='bodyinput' name='body'></textarea>");
+      // // A button to submit a new note, with the id of the article saved to it
+      // $("#myModal").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+
+      // // If there's a note in the article
+      // if (data.note) {
+      //   // Place the title of the note in the title input
+      //   $("#titleinput").val(data.note.title);
+      //   // Place the body of the note in the body textarea
+      //   $("#bodyinput").val(data.note.body);
+      // }
+//     });
+// });
+
+// // When you click the savenote button
+// $(document).on("click", "#savenote", function() {
+//   // Grab the id associated with the article from the submit button
+//   var thisId = $(this).attr("data-id");
+
+//   // Run a POST request to change the note, using what's entered in the inputs
+//   $.ajax({
+//     method: "POST",
+//     url: "/articles/" + thisId,
+//     data: {
+//       // Value taken from title input
+//       title: $("#titleinput").val(),
+//       // Value taken from note textarea
+//       body: $("#bodyinput").val()
+//     }
+//   })
+//     // With that done
+//     .done(function(data) {
+//       // Log the response
+//       //console.log(data);
+//       // Empty the notes section
+//       $("#notes").empty();
+//     });
+
+//   // Also, remove the values entered in the input and textarea for note entry
+//   $("#titleinput").val("");
+//   $("#bodyinput").val("");
+// });
